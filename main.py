c7b6ad0d75f8db24fed960ed8298159fdcbed84f
poikNplop <hrmorley3.4@gmail.com>
2020-03-08 15:13:59 +0000

    Presence and proper quitting

--- main.py
+++ main.py
@@ -7,17 +7,28 @@ import json
 
 bot = commands.Bot(command_prefix="&")
 
+activity = discord.Game("wow - &wow")
+
 @bot.event
 async def on_ready():
     print("Logged in as {0} ({0.id})".format(bot.user))
+    await bot.change_presence(activity=activity)
+    print(f"Updated status: {activity}")
 
 bot.load_extension("sounds")
 @bot.command()
 async def reload(ctx):
     bot.reload_extension("sounds")
     print("Reloaded")
+    await bot.change_presence(activity=activity)
+    print(f"Updated status: {activity}")
 
 
 with open("client_data.json", "r") as f:
     client_data = json.load(f)
-bot.run(client_data["token"])
+
+try:
+    bot.loop.run_until_complete(bot.start(client_data["token"]))
+except KeyboardInterrupt:
+    print("Stopping... (^C)")
+    bot.loop.run_until_complete(bot.close())
