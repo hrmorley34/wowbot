77091a65a90da204ba3257b909eb3a00e88ea54c
poikNplop <hrmorley3.4@gmail.com>
2020-03-11 17:47:37 +0000

    Move names/locations to JSON file

--- sounds.py
+++ sounds.py
@@ -2,70 +2,90 @@ import discord
 from discord.ext import commands
 
 import os
+import json
 import glob
 import random
 
-class Sounds(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+@commands.command(name="join")
+async def join_voice(ctx):
+    voicestate = ctx.author.voice
 
+    if voicestate is None:
+        await ctx.send("But you aren't in a voice chat!")
+        return False
 
-    @commands.command(name="join")
-    async def join_voice(self, ctx):
-        voicestate = ctx.author.voice
-
-        if voicestate is None:
-            await ctx.send("But you aren't in a voice chat!")
-            return False
-
-        if ctx.voice_client and ctx.voice_client.channel == voicestate.channel: # already there
-            if ctx.voice_client.is_playing():
-                ctx.voice_client.stop()
-            return True
-
-        try:
-            await voicestate.channel.connect()
-        except discord.ClientException: # already in a voice chat
-            if ctx.voice_client and not ctx.voice_client.is_playing():
-                await self.move_to(voicestate.channel)
-            else:
-                await ctx.send("Sorry, I'm busy right now.")
-                return False
-
+    if ctx.voice_client and ctx.voice_client.channel == voicestate.channel: # already there
+        if ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
         return True
 
-    async def play_random(self, ctx, filenames):
-        if len(filenames) <= 0:
-            await ctx.send("I can't find any files...")
-            return
-        fname = random.choice(filenames)
-        source = await discord.FFmpegOpusAudio.from_probe(fname)
+    try:
+        await voicestate.channel.connect()
+    except discord.ClientException: # already in a voice chat
+        if ctx.voice_client and not ctx.voice_client.is_playing():
+            await ctx.bot.move_to(voicestate.channel)
+        else:
+            await ctx.send("Sorry, I'm busy right now.")
+            return False
 
-        ctx.voice_client.play(source)
+    return True
 
-        return fname
+async def play_random(ctx, filenames):
+    if len(filenames) <= 0:
+        await ctx.send("I can't find any files...")
+        return
+    fname = random.choice(filenames)
+    source = await discord.FFmpegOpusAudio.from_probe(fname)
 
-    @commands.command(name="leave")
-    async def leave_voice(self, ctx):
-        if ctx.voice_client:
-            await ctx.voice_client.disconnect()
+    ctx.voice_client.play(source)
 
+    return fname
 
-    @commands.command()
-    async def wow(self, ctx):
-        if await self.join_voice(ctx):
-            fname = await self.play_random(ctx, glob.glob("sounds/wow*.*"))
-            print(f"Played {fname} in {ctx.channel}")
-
-    @commands.command()
-    async def death(self, ctx):
-        if await self.join_voice(ctx):
-            fname = await self.play_random(ctx, glob.glob("sounds/legostarwarsdeath-*.*"))
-            print(f"Played {fname} in {ctx.channel}")
+@commands.command(name="leave")
+async def leave_voice(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
 
 
+def sound_player(name, files):
+    @commands.command(name=name)
+    async def function(ctx):
+        if await join_voice(ctx):
+            w = ([], [])
+            for fd in files:
+                if "glob" in fd.keys():
+                    filenames = glob.glob(fd["glob"])
+                elif "filenames" in fd.keys():
+                    filenames = list(fd["filenames"])
+                else:
+                    filenames = [fd["filename"]]
+                weight = fd.get("weight", 1)
+
+                w[0].append(filenames)
+                w[1].append(weight)
+
+            fnames = random.choices(*w)[0]
+            fname = await play_random(ctx, fnames)
+            print(f"Played {name} ({fname}) in {ctx.channel}")
+
+    return function
+
+
+LOADED_COMMANDS = []
 def setup(bot):
-    bot.add_cog(Sounds(bot))
+    bot.add_command(join_voice)
+    bot.add_command(leave_voice)
+
+    with open("sounds/sounds.json") as f:
+        SOUNDS = json.load(f)
+
+    for name, files in SOUNDS.items():
+        bot.add_command(sound_player(name, files))
+        LOADED_COMMANDS.append(name)
 
 def teardown(bot):
-    bot.remove_cog("Sounds")
+    bot.remove_command(join_voice)
+    bot.remove_command(leave_voice)
+
+    for name in LOADED_COMMANDS:
+        bot.remove_command(name)
