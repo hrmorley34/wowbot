f903acf2e654b043cb7734f67fbd8b5d88e57654
hrmorley34 <henry@morley.org.uk>
2021-01-20 16:43:30 +0000

    Move cmds/listeners from main to __init__ cog

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,9 +1,52 @@
+import discord
 from discord.ext import commands
+
+import random
+
+
+activities = [discord.Game("wow - &wow")]
+
+
+class InitCog(commands.Cog):
+    bot: commands.bot.BotBase
+
+    def __init__(self, bot: commands.bot.BotBase):
+        self.bot = bot
+
+    async def update_status(self):
+        act = random.choice(activities)
+        await self.bot.change_presence(activity=act)
+        print(f"Updated status: {act}")
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Logged in as {0} ({0.id})".format(self.bot.user))
+
+        await self.update_status()
+
+    @commands.command()
+    @commands.is_owner()
+    async def reload(self, ctx):
+        self.bot.reload_extension("cogs")
+        print("Reloaded")
+
+        try:
+            # \N{White heavy check mark}
+            await ctx.message.add_reaction("\u2705")
+        except discord.Forbidden:
+            pass  # it doesn't matter too much
+
+        await self.update_status()
 
 
 def setup(bot):
+    bot.add_cog(InitCog(bot))
     for ext in ["cogs.sounds", "cogs.reactor"]:
         try:
             bot.load_extension(ext)
         except commands.ExtensionAlreadyLoaded:
             bot.reload_extension(ext)
+
+
+def teardown(bot: commands.bot.BotBase):
+    bot.remove_cog("InitCog")
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import discord
 from discord.ext import commands
 
 import json
@@ -7,32 +6,8 @@ import json
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("&"))
 
-activity = discord.Game("wow - &wow")
-
-
-@bot.event
-async def on_ready():
-    print("Logged in as {0} ({0.id})".format(bot.user))
-    await bot.change_presence(activity=activity)
-    print(f"Updated status: {activity}")
-
 
 bot.load_extension("cogs")
-
-
-@bot.command()
-@commands.is_owner()
-async def reload(ctx):
-    bot.reload_extension("cogs")
-    print("Reloaded")
-
-    try:
-        await ctx.message.add_reaction("\u2705")  # \N{White heavy check mark}
-    except discord.Forbidden:
-        pass  # it doesn't matter too much
-
-    await bot.change_presence(activity=activity)
-    print(f"Updated status: {activity}")
 
 
 with open("client_data.json", "r") as f:
