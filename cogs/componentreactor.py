97c6b006f0bde8cd1258d2512fc1f28579f90180
hrmorley34 <henry@morley.org.uk>
2021-10-13 22:32:04 +0100

    Add react reload command

--- cogs/componentreactor.py
+++ cogs/componentreactor.py
@@ -1,7 +1,6 @@
 "Component Reactor: click on components to play sounds"
 
 from __future__ import annotations
-import functools
 
 import discord
 from discord.channel import TextChannel
@@ -13,7 +12,7 @@ from discord_slash.context import ComponentContext, SlashContext
 from discord_slash.model import ButtonStyle
 from discord_slash.utils.manage_components import create_button, create_actionrow
 import traceback
-from typing import TYPE_CHECKING, Any, Callable
+from typing import TYPE_CHECKING
 from .utils import JsonFileDict, Problem
 
 if TYPE_CHECKING:
@@ -28,7 +27,7 @@ class ComponentReactorCog(commands.Cog):
     bot: commands.Bot
     guilds: JsonFileDict[int, ReactionGuild]
 
-    def get_embed(self, ctx: commands.Context) -> discord.Embed:
+    def get_embed(self, ctx: SlashContext) -> discord.Embed:
         em = discord.Embed(title="React here to play sound!")
         return em
 
@@ -62,7 +61,7 @@ class ComponentReactorCog(commands.Cog):
         self.bot = bot
         self.guilds = JsonFileDict("jsons/componentreactionguilds.json")
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "<{}>".format(type(self).__name__)
 
     async def get_guild_message(self, rg: ReactionGuild) -> Message:
@@ -76,7 +75,7 @@ class ComponentReactorCog(commands.Cog):
     # async def help(self, ctx: SlashContext):
     #     pass
 
-    @cog_ext.cog_subcommand(base=BASE_COMMAND, name="here", guild_ids=[690860332322914305])
+    @cog_ext.cog_subcommand(base=BASE_COMMAND, name="here")
     async def here(self, ctx: SlashContext):
         " Create a reactable message in the current text channel "
         if ctx.guild is None:
@@ -84,7 +83,7 @@ class ComponentReactorCog(commands.Cog):
 
         await self.in_.invoke(ctx, ctx.channel)
 
-    @cog_ext.cog_subcommand(base=BASE_COMMAND, name="in", guild_ids=[690860332322914305])
+    @cog_ext.cog_subcommand(base=BASE_COMMAND, name="in")
     async def in_(self, ctx: SlashContext, channel: TextChannel):
         " Add a reactor in the specified channel "
         message: Message = await channel.send(embed=self.get_embed(ctx), components=self.get_components())
@@ -104,7 +103,7 @@ class ComponentReactorCog(commands.Cog):
 
         await ctx.send("Sent!", hidden=True)
 
-    @cog_ext.cog_subcommand(base=BASE_COMMAND, name="remove", guild_ids=[690860332322914305])
+    @cog_ext.cog_subcommand(base=BASE_COMMAND, name="remove")
     async def remove(self, ctx: SlashContext):
         " Remove this server's reactable message "
         if ctx.guild is None:
@@ -125,26 +124,19 @@ class ComponentReactorCog(commands.Cog):
         else:
             await ctx.send("There isn't one!", hidden=True)
 
-    # @cog_ext.cog_subcommand(base=BASE_COMMAND, name="reload")
-    # async def reload(self, ctx: SlashContext):
-    #     " Reset the reactions "
-    #     if ctx.guild is None:
-    #         raise Problem("You aren't in a server!")
+    @cog_ext.cog_subcommand(base=BASE_COMMAND, name="reload")
+    async def reload(self, ctx: SlashContext):
+        " Reset the reactions "
+        if ctx.guild is None:
+            raise Problem("You aren't in a server!")
 
-    #     gdata = self.guilds.get(ctx.guild.id)
+        gdata = self.guilds.get(ctx.guild.id)
+        if gdata is None:
+            raise Problem("There's no message in this server!")
 
-    #     if gdata is None:
-    #         raise Problem("There's no message in this server!")
-
-    #     ch = self.bot.get_channel(gdata["channel"])
-    #     if ch is None:
-    #         ch = await self.bot.fetch_channel(gdata["channel"])
-    #     msg = await ch.fetch_message(gdata["message"])
-    #     await self.remove_reactions(msg)
-
-    #     await self.add_reactions(msg)
-
-    #     await react_output(self.bot, ctx.message)
+        msg = await self.get_guild_message(gdata)
+        await msg.edit(embed=self.get_embed(ctx), components=self.get_components())
+        await ctx.send("Done!", hidden=True)
 
     @commands.Cog.listener(name="on_component")
     async def _on_component(self, ctx: ComponentContext):
